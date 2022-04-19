7e073e6a706782970d6b4fe9c06278dd72c9f18c
anthonygonzales <anthony.j.gonzales22@gmail.com>
2022-04-19 11:01:56 -0500

    finished views exercise

--- src/main/java/com/codeup/springblog/PostController.java
+++ src/main/java/com/codeup/springblog/PostController.java
@@ -1,22 +1,34 @@
 package com.codeup.springblog;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class PostController {
     @GetMapping("/posts")
-    @ResponseBody
-    public String index(){
-        return "All blog posts will be here";
+    public String index(Model model){
+        Post post1 = new Post("helllooo", "im all out of bubblegum");
+        Post post2 = new Post("in the livingroom", "in the kitchen");
+        ArrayList <Post> postList = new ArrayList<>();
+        postList.add(post1);
+        postList.add(post2);
+        model.addAttribute("posts", postList);
+        return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String post(@PathVariable String id){
-        return "A single post will be shown here.";
+    public String post(@PathVariable String id, Model model){
+        Post post1 = new Post("New title", "hello from this new post");
+        model.addAttribute("postTitle", post1.getTitle());
+        model.addAttribute("postBody", post1.getBody());
+        model.addAttribute("postid", id);
+        return "posts/show";
     }
 
     @GetMapping("/posts/create")
